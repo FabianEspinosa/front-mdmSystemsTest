47166d357fa4f080ff745474bf9c5d197c31982b
Fabian Espinosa <Fabianespinosa1988@gmail.com>
2023-02-26 14:28:18 +0100

    undo change

--- src/router.jsx
+++ src/router.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                element: <Navigate to="/login" />,
+                element: <Navigate to="/dashboard" />,
             },
             {
                 path: "/dashboard",
